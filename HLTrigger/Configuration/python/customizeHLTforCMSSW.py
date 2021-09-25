7d7525f7949c0f0379f9dc7a772f1e7e8b08388e
Marino Missiroli <marino.missiroli@cern.ch>
2021-09-25 09:01:31 +0200

    fixes for fillDescriptions in CkfTrackCandidateMaker and CkfTrajectoryMaker (wip)

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -140,6 +140,33 @@ def customiseFor35315(process):
 
     return process
 
+def customiseFor35385(process):
+    """Update the HLT configuration for the changes in #35385:
+    Introduction of fillDescriptions for CkfTrackCandidateMaker and CkfTrajectoryMaker
+    """
+    for iMod in producers_by_type(process, 'CkfTrackCandidateMaker'):
+        for aPar in ['SimpleMagneticField', 'TrajectoryBuilder']:
+            if hasattr(iMod, aPar): delattr(iMod, aPar)
+
+        if not hasattr(iMod, 'maxSeedsBeforeCleaning'):
+            iMod.maxSeedsBeforeCleaning = cms.uint32(0)
+
+    for iMod in producers_by_type(process, 'CkfTrajectoryMaker'):
+        for aPar in ['TrajectoryBuilder']:
+            if hasattr(iMod, aPar): delattr(iMod, aPar)
+
+    for aPSet in process._Process__psets.values():
+        if hasattr(aPSet, 'ComponentType') and aPSet.ComponentType in ['CkfTrajectoryBuilder', 'GroupedCkfTrajectoryBuilder', 'MuonCkfTrajectoryBuilder']:
+            for aPar in ['MeasurementTrackerName', 'cleanTrajectoryAfterInOut', 'doSeedingRegionRebuilding', 'useHitsSplitting']:
+               if hasattr(aPSet, aPar): delattr(aPSet, aPar)
+
+            if aPSet.ComponentType == 'GroupedCkfTrajectoryBuilder' and aPSet.useSameTrajFilter:
+                if not hasattr(aPSet, 'inOutTrajectoryFilter'): aPSet.inOutTrajectoryFilter = aPSet.trajectoryFilter.clone()
+
+            if aPSet.ComponentType == 'CkfTrajectoryBuilder' and hasattr(aPSet, 'minNrOfHitsForRebuild'): delattr(aPSet, 'minNrOfHitsForRebuild')
+
+    return process
+
 # CMSSW version specific customizations
 def customizeHLTforCMSSW(process, menuType="GRun"):
     
@@ -151,5 +178,6 @@ def customizeHLTforCMSSW(process, menuType="GRun"):
     # process = customiseFor12718(process)
 
     process = customiseFor35315(process)
+    process = customiseFor35385(process)
 
     return process
