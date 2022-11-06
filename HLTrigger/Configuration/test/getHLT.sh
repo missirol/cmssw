a627cd0c0f3fb68b18af4e44769a9dfad036b6ac
Marino Missiroli <marino.missiroli@cern.ch>
2022-11-07 00:19:34 +0100

    enable use of dbproxy in more HLT-dev tools

--- HLTrigger/Configuration/test/getHLT.sh
+++ HLTrigger/Configuration/test/getHLT.sh
@@ -1,66 +1,59 @@
-#! /bin/bash
+#!/bin/bash
 
 # ConfDB configurations to use
-MASTER="/dev/CMSSW_12_4_0/HLT"             # no explicit version, take the most recent
-TARGET="/dev/CMSSW_12_4_0/\$TABLE"         # no explicit version, take the most recent
+MASTER="/dev/CMSSW_12_4_0/HLT"      # no explicit version, take the most recent
+TARGET="/dev/CMSSW_12_4_0/\$TABLE"  # no explicit version, take the most recent
 
-TABLES="GRun HIon PIon PRef"               # $TABLE in the above variable will be expanded to these TABLES
+TABLES="GRun HIon PIon PRef"        # $TABLE in the above variable will be expanded to these TABLES
 
-# print extra messages ?
-VERBOSE=false
+# command-line arguments
+VERBOSE=false # print extra messages to stdout
+DBPROXYOPTS="" # db-proxy configuration
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -v) VERBOSE=true; shift;;
+    -q) VERBOSE=false; shift;;
+    --dbproxy) DBPROXYOPTS="${DBPROXYOPTS} --dbproxy"; shift;;
+    --dbproxyhost) DBPROXYOPTS="${DBPROXYOPTS} --dbproxyhost $2"; shift; shift;;
+    --dbproxyport) DBPROXYOPTS="${DBPROXYOPTS} --dbproxyport $2"; shift; shift;;
+    *) shift;;
+  esac
+done
+
+# remove spurious whitespaces and tabs from DBPROXYOPTS
+DBPROXYOPTS=$(echo "${DBPROXYOPTS}" | xargs)
+
+# path to directory hosting this script
+TESTDIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
 # this is used for brace expansion
 TABLES_=$(echo $TABLES | sed -e's/ \+/,/g')
-
-[ "$1" == "-v" ] && { VERBOSE=true;  shift; }
-[ "$1" == "-q" ] && { VERBOSE=false; shift; }
 
 function log() {
   $VERBOSE && echo -e "$@"
 }
 
-function findHltScript() {
-  local PACKAGE="HLTrigger/Configuration"
-  local SCRIPT="$1"
-
-  if [ -f "$SCRIPT" ]; then
-    echo "./$SCRIPT"
-  elif [ -f "$CMSSW_BASE/src/$PACKAGE/test/$SCRIPT" ]; then
-    echo "$CMSSW_BASE/src/$PACKAGE/test/$SCRIPT"
-  elif [ -f "$CMSSW_RELEASE_BASE/src/$PACKAGE/test/$SCRIPT" ]; then
-    echo "$CMSSW_RELEASE_BASE/src/$PACKAGE/test/$SCRIPT"
-  else
-    echo "cannot find $SCRIPT, aborting" 
-    exit 1
-  fi
-}
-
-GETCONTENT=$(findHltScript getEventContent.py)
-GETDATASETS=$(findHltScript getDatasets.py)
-
-function getConfigForCVS() {
+function getConfig() {
   local CONFIG="$1"
   local NAME="$2"
   log "  dumping HLT cffs for $NAME from $CONFIG"
 
   # do not use any conditions or L1 override
-  hltGetConfiguration --cff --data $CONFIG --type $NAME > HLT_${NAME}_cff.py
+  hltGetConfiguration --cff --data ${CONFIG} --type ${NAME} ${DBPROXYOPTS} > HLT_${NAME}_cff.py
 }
 
-function getContentForCVS() {
-  local CONFIG="$1"
-
-  log "  dumping EventContet"
-  $GETCONTENT $CONFIG
-  rm -f hltOutput*_cff.py* hltScouting_cff.py*
-}
-
-function getDatasetsForCVS() {
+function getEventContent() {
+  log "  dumping EventContent"
   local CONFIG="$1"
   local TARGET="$2"
+  ${TESTDIR}/getEventContent.py ${CONFIG} ${DBPROXYOPTS} > ${TARGET}
+}
 
+function getDatasets() {
   log "  dumping Primary Dataset"
-  $GETDATASETS $CONFIG > $TARGET
+  local CONFIG="$1"
+  local TARGET="$2"
+  ${TESTDIR}/getDatasets.py ${CONFIG} ${DBPROXYOPTS} > ${TARGET}
 }
 
 function getConfigForOnline() {
@@ -83,51 +76,51 @@ function getConfigForOnline() {
   local L1TPP1=""
   local L1TPP2=""
 
-  log "  dumping full HLT for $NAME from $CONFIG"
+  log "  dumping full HLT for ${NAME} from ${CONFIG}"
+
   # override L1 menus
-  if [ "$NAME" == "Fake" ]; then
-    hltGetConfiguration --full --data $CONFIG --type $NAME --unprescale --process HLT$NAME --globaltag "auto:run1_hlt_${NAME}" --input "file:RelVal_Raw_${NAME}_DATA.root" > OnLine_HLT_$NAME.py
-  elif [ "$NAME" == "Fake1" ] || [ "$NAME" == "Fake2" ] || [ "$NAME" == "2018" ]; then
-    hltGetConfiguration --full --data $CONFIG --type $NAME --unprescale --process HLT$NAME --globaltag "auto:run2_hlt_${NAME}" --input "file:RelVal_Raw_${NAME}_DATA.root" > OnLine_HLT_$NAME.py
-  else
-    hltGetConfiguration --full --data $CONFIG --type $NAME --unprescale --process HLT$NAME --globaltag "auto:run3_hlt_${NAME}" --input "file:RelVal_Raw_${NAME}_DATA.root" > OnLine_HLT_$NAME.py
+  local AUTOGT="auto:run3_hlt_${NAME}"
+  if [ "${NAME}" = "Fake1" ] || [ "${NAME}" = "Fake2" ] || [ "${NAME}" = "2018" ]; then
+    AUTOGT="auto:run2_hlt_${NAME}"
+  elif [ "${NAME}" = "Fake" ]; then
+    AUTOGT="auto:run1_hlt_${NAME}"
   fi
 
+  hltGetConfiguration --full --data "${CONFIG}" --type "${NAME}" --unprescale --process "HLT${NAME}" --globaltag "${AUTOGT}" \
+    --input "file:RelVal_Raw_${NAME}_DATA.root" ${DBPROXYOPTS} > OnLine_HLT_"${NAME}".py
 }
 
 # make sure we're using *this* working area
-eval `scramv1 runtime -sh`
-hash -r
+eval `scram runtime -sh`
 
-# cff python dumps, in CVS under HLTrigger/Configuration/pyhon
-log "Extracting cff python dumps"
+# cff fragments in HLTrigger/Configuration/python/
 echo "Extracting cff python dumps"
 FILES=$(eval echo HLT_FULL_cff.py HLT_{$TABLES_}_cff.py HLTrigger_Datasets_{$TABLES_}_cff.py HLTrigger_EventContent_cff.py )
-rm -f $FILES
-getConfigForCVS  $MASTER FULL
-getContentForCVS $MASTER
-for TABLE in $TABLES; do
-  log "$TABLE"
-  echo "$TABLE"
-  getConfigForCVS $(eval echo $TARGET) $TABLE
-  getDatasetsForCVS $(eval echo $TARGET) HLTrigger_Datasets_${TABLE}_cff.py
+rm -f ${FILES}
+getConfig ${MASTER} FULL
+getEventContent ${MASTER} HLTrigger_EventContent_cff.py
+for TABLE in ${TABLES}; do
+  log "${TABLE}"
+  echo "${TABLE}"
+  getConfig $(eval echo ${TARGET}) ${TABLE}
+  getDatasets $(eval echo ${TARGET}) HLTrigger_Datasets_${TABLE}_cff.py
 done
 log "Done"
-log "$(ls -l $FILES)"
-mv -f $FILES ../python/
+log "$(ls -l ${FILES})"
+mv -f ${FILES} ../python/
 log
 
-# full config dumps, in CVS under HLTrigger/Configuration/test
+# full configs in HLTrigger/Configuration/test/
 log "Extracting full configuration dumps"
 echo "Extracting full configuration dumps"
 FILES=$(eval echo OnLine_HLT_FULL.py OnLine_HLT_{$TABLES_}.py)
-rm -f $FILES
-getConfigForOnline $MASTER FULL
-for TABLE in $TABLES; do
-  log "$TABLE"
-  echo "$TABLE"
-  getConfigForOnline $(eval echo $TARGET) $TABLE
+rm -f ${FILES}
+getConfigForOnline ${MASTER} FULL
+for TABLE in ${TABLES}; do
+  log "${TABLE}"
+  echo "${TABLE}"
+  getConfigForOnline $(eval echo ${TARGET}) ${TABLE}
 done
 log "Done"
-log "$(ls -l $FILES)"
+log "$(ls -l ${FILES})"
 log
