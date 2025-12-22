bb82c488ff5633562e1ef083299588ee30f4047c
Marino Missiroli <marino.missiroli@cern.ch>
2025-12-22 16:38:43 +0100

    L1-uGT emulator: fix to AXOL1TL's "Pt" inputs

--- L1Trigger/L1TGlobal/src/AXOL1TLCondition.cc
+++ L1Trigger/L1TGlobal/src/AXOL1TLCondition.cc
@@ -180,7 +180,7 @@ const bool l1t::AXOL1TLCondition::evaluateCondition(const int bxEval) const {
     for (int iEtSum = 0; iEtSum < NCandEtSum; iEtSum++) {
       if ((candEtSumVec->at(useBx, iEtSum))->getType() == l1t::EtSum::EtSumType::kMissingEt) {
         EtSumInput[0] =
-            ((candEtSumVec->at(useBx, iEtSum))->hwPt()) / 2;  //have to do hwPt/2 in order to match original et inputs
+            ((candEtSumVec->at(useBx, iEtSum))->hwPt()) / 2.;  //have to do hwPt/2 in order to match original et inputs
         // EtSumInput[1] = (candEtSumVec->at(useBx, iEtSum))->hwEta(); //this one is zero, so leave it zero
         EtSumInput[2] = (candEtSumVec->at(useBx, iEtSum))->hwPhi();
       }
@@ -192,7 +192,7 @@ const bool l1t::AXOL1TLCondition::evaluateCondition(const int bxEval) const {
     for (int iEG = 0; iEG < NCandEG; iEG++) {
       if (iEG < NEgammas) {  //stop if fill the Nobjects we need
         EgammaInput[0 + (3 * iEG)] = ((candEGVec->at(useBx, iEG))->hwPt()) /
-                                     2;  //index 0,3,6,9 //have to do hwPt/2 in order to match original et inputs
+                                     2.;  //index 0,3,6,9 //have to do hwPt/2 in order to match original et inputs
         EgammaInput[1 + (3 * iEG)] = (candEGVec->at(useBx, iEG))->hwEta();  //index 1,4,7,10
         EgammaInput[2 + (3 * iEG)] = (candEGVec->at(useBx, iEG))->hwPhi();  //index 2,5,8,11
       }
@@ -204,7 +204,7 @@ const bool l1t::AXOL1TLCondition::evaluateCondition(const int bxEval) const {
     for (int iMu = 0; iMu < NCandMu; iMu++) {
       if (iMu < NMuons) {  //stop if fill the Nobjects we need
         MuInput[0 + (3 * iMu)] = ((candMuVec->at(useBx, iMu))->hwPt()) /
-                                 2;  //index 0,3,6,9 //have to do hwPt/2 in order to match original et inputs
+                                 2.;  //index 0,3,6,9 //have to do hwPt/2 in order to match original et inputs
         MuInput[1 + (3 * iMu)] = (candMuVec->at(useBx, iMu))->hwEtaAtVtx();  //index 1,4,7,10
         MuInput[2 + (3 * iMu)] = (candMuVec->at(useBx, iMu))->hwPhiAtVtx();  //index 2,5,8,11
       }
@@ -216,7 +216,7 @@ const bool l1t::AXOL1TLCondition::evaluateCondition(const int bxEval) const {
     for (int iJet = 0; iJet < NCandJet; iJet++) {
       if (iJet < NJets) {  //stop if fill the Nobjects we need
         JetInput[0 + (3 * iJet)] = ((candJetVec->at(useBx, iJet))->hwPt()) /
-                                   2;  //index 0,3,6,9...27 //have to do hwPt/2 in order to match original et inputs
+                                   2.;  //index 0,3,6,9...27 //have to do hwPt/2 in order to match original et inputs
         JetInput[1 + (3 * iJet)] = (candJetVec->at(useBx, iJet))->hwEta();  //index 1,4,7,10...28
         JetInput[2 + (3 * iJet)] = (candJetVec->at(useBx, iJet))->hwPhi();  //index 2,5,8,11...29
       }
