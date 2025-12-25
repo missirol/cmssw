d3070a639c869fc661d3336e39c2091d4f3cc5d4
Marino Missiroli <marino.missiroli@cern.ch>
2025-12-25 01:18:05 +0100

    updated unit test testHLS4MLModelWrapper

---
+++ L1Trigger/MLUtilities/test/TestHLS4MLModelB.cc
@@ -0,0 +1,30 @@
+#include <any>
+#include <array>
+
+#include "L1Trigger/MLUtilities/interface/HLS4MLModelWrapper.h"
+
+class TestHLS4MLModelB : public l1t::HLS4MLModel {
+private:
+  using inputs_t = std::array<int, 3>;
+  using output_t = int;
+  inputs_t inputs_;
+  output_t output_;
+
+public:
+  TestHLS4MLModelB() : inputs_{0, 0, 0}, output_{0} {}
+
+  virtual ~TestHLS4MLModelB() = default;
+
+  virtual void prepare_input(std::any input) { inputs_ = std::any_cast<inputs_t>(input); }
+
+  virtual void predict() { output_ = inputs_[0] + inputs_[1] * inputs_[2]; }
+
+  virtual void read_result(std::any result) {
+    output_t* output = std::any_cast<output_t*>(result);
+    *output = output_;
+  }
+};
+
+extern "C" l1t::HLS4MLModel* create_model() { return new TestHLS4MLModelB; }
+
+extern "C" void destroy_model(l1t::HLS4MLModel* m) { delete m; }
--- L1Trigger/MLUtilities/test/testHLS4MLModelWrapper.cc
+++ L1Trigger/MLUtilities/test/testHLS4MLModelWrapper.cc
@@ -1,18 +1,60 @@
-#include <iostream>
+#include <array>
+#include <string>
+
+#include <catch2/catch_test_macros.hpp>
 
 #include "L1Trigger/MLUtilities/interface/HLS4MLModelWrapper.h"
 
-int main(int, char**) {
-  l1t::HLS4MLModelWrapper mw_a{"GTADModel_v1"};
-  l1t::HLS4MLModelWrapper mw_b{"GTADModel_v1"};
-  l1t::HLS4MLModelWrapper mw_c{"GTADModel_v3"};
+TEST_CASE("HLS4MLModelWrapper basic functionalities", "[HLS4MLModelWrapper]") {
+  std::string const libA{"libL1TriggerMLUtilitiesTestHLS4MLModelA"};
+  std::string const libB{"libL1TriggerMLUtilitiesTestHLS4MLModelB"};
 
-  mw_a.reset();
-  mw_b.reset("GTADModel_v3");
+  l1t::HLS4MLModelWrapper mw1{libA};
+  l1t::HLS4MLModelWrapper mw2{libB};
+  l1t::HLS4MLModelWrapper mw3{libA};
 
-  mw_a = std::move(mw_c);
-  mw_a.reset("GTADModel_v3");
-  mw_a.reset("GTADModel_v1");
+  std::array<int, 3> mw1_inputs{1, 2, 3};
+  std::array<int, 3> mw2_inputs{4, 5, 6};
+  std::array<int, 3> mw3_inputs{7, 8, 9};
 
-  return 0;
+  int mw1_output{0};
+  int mw2_output{0};
+  int mw3_output{0};
+
+  SECTION("Run inference for two different models") {
+    mw1.prepare_input(mw1_inputs);
+    mw1.predict();
+    mw1.read_result(&mw1_output);
+    REQUIRE(mw1_output == 5);
+
+    mw2.prepare_input(mw2_inputs);
+    mw2.predict();
+    mw2.read_result(&mw2_output);
+    REQUIRE(mw2_output == 34);
+  }
+
+  SECTION("Run inference for two instances of the same model") {
+    mw1.prepare_input(mw1_inputs);
+    mw3.prepare_input(mw3_inputs);
+
+    mw1.predict();
+    mw3.predict();
+
+    mw1.read_result(&mw1_output);
+    mw3.read_result(&mw3_output);
+
+    REQUIRE(mw1_output == 5);
+    REQUIRE(mw3_output == 65);
+  }
+
+  SECTION("Reset a model wrapper") { REQUIRE_NOTHROW(mw1.reset()); }
+
+  SECTION("Change model, and rerun inference") {
+    mw2.reset(libA);
+
+    mw2.prepare_input(mw2_inputs);
+    mw2.predict();
+    mw2.read_result(&mw2_output);
+    REQUIRE(mw2_output == 26);
+  }
 }
