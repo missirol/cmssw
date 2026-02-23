d710086b9fbe0b7f873ebfa0d2ee86577c55f7c1
Marino Missiroli <marino.missiroli@cern.ch>
2026-02-23 08:31:17 +0100

    L1S: bugfix for CaloTower eta/phi conversion in ScoutingJetProducer
    Fixed a bug in the conversion of the CaloTower
    (ieta, iphi) integer values to physical (eta, phi) values.
    
     - For the "ieta to eta" conversion,
       a tower dimension of 0.087 in eta was being used,
       but this is correct only for |ieta| <= 20,
       while towers at higher |ieta| have a larger size
       (see, for example, Table 1 of CMS Note 2005/016).
    
     - For both the "ieta to eta" and "iphi to phi" conversion,
       the current implementation leads to using the coordinates corresponding
       to one edge of the tower (e.g. the eta of the outer edge),
       rather than those corresponding to the center of the tower.
    
    For more info, see the comments in the implementation of the functions
    l1ScoutingRun3::calol1::fEta() and l1ScoutingRun3::calol1::fPhi() in
     L1TriggerScouting/Utilities/src/conversion.cc
    
    In addition, the type of the argument of the
    l1ScoutingRun3::calol1::{fEt,fEta,fPhi}() functions
    is changed from "int" to "int16_t" in order to match
    the type used in the l1ScoutingRun3::CaloTower data format
    (as noted by Rocco A.).
    
    Co-Authored-By: Rocco Ardino <rocco.ardino@cern.ch>

--- L1TriggerScouting/OnlineProcessing/plugins/ScoutingJetProducer.cc
+++ L1TriggerScouting/OnlineProcessing/plugins/ScoutingJetProducer.cc
@@ -11,12 +11,9 @@
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "L1TriggerScouting/Utilities/interface/conversion.h"
 
-// ROOT libraries
-#include "Math/Vector4D.h"
-#include "Math/VectorUtil.h"
-
-// fastjet libraries
 #include "fastjet/ClusterSequence.hh"
+#include "fastjet/JetDefinition.hh"
+#include "fastjet/PseudoJet.hh"
 
 class ScoutingJetProducer : public edm::global::EDProducer<> {
 public:
@@ -78,11 +75,11 @@ void ScoutingJetProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::
         continue;
       }
 
-      ROOT::Math::PtEtaPhiMVector ctLV(l1ScoutingRun3::calol1::fEt(ct.hwEt()),
-                                       l1ScoutingRun3::calol1::fEta(ct.hwEta()),
-                                       l1ScoutingRun3::calol1::fPhi(ct.hwPhi()),
-                                       0);
-      pjCTs.emplace_back(ctLV.px(), ctLV.py(), ctLV.pz(), ctLV.E());
+      float const ctEt = l1ScoutingRun3::calol1::fEt(ct.hwEt());
+      float const ctEta = l1ScoutingRun3::calol1::fEta(ct.hwEta());
+      float const ctPhi = l1ScoutingRun3::calol1::fPhi(ct.hwPhi());
+
+      pjCTs.emplace_back(fastjet::PtYPhiM(ctEt, ctEta, ctPhi, 0));
     }
 
     // run the jet clustering with the given jet definition
