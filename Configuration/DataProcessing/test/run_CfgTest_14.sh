c3d6570d611a10f012d88dce0e572fd536320ae0
Marino Missiroli <marino.missiroli@cern.ch>
2026-03-07 17:55:17 +0100

    L1S(Nano): introduce L1-Scouting "scenario" for 2026 data-taking
    This adds a scenario named "l1ScoutingEra_Run3_2026",
    in order to produce "L1ScoutingNano" data sets at Tier-0 in 2026.
    
    This closely resembles what has been done for HLT-Scouting since 2024, see for example
    https://github.com/cms-sw/cmssw/pull/44970

---
+++ Configuration/DataProcessing/test/run_CfgTest_14.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Test suite for various ConfigDP scenarios
+# run using: scram build runtests
+# feel free to contribute with your favourite configuration
+
+
+# Pass in name and status
+function die { echo $1: status $2 ;  exit $2; }
+
+function runTest { echo $1 ; python3 $1 || die "Failure for configuration: $1" $?; }
+
+declare -a arr=("l1ScoutingEra_Run3_2026")
+for scenario in "${arr[@]}"
+do
+     runTest "${SCRAM_TEST_PATH}/RunPromptReco.py --scenario $scenario --nanoaod --global-tag GLOBALTAG --lfn=/store/whatever --nanoFlavours=@L1Scout"
+     runTest "${SCRAM_TEST_PATH}/RunPromptReco.py --scenario $scenario --nanoaod --global-tag GLOBALTAG --lfn=/store/whatever --nanoFlavours=@L1ScoutSelect"
+done
