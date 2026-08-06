acba719999866fcb5298f21783a10715bd135f5b
Marino Missiroli <marino.missiroli@cern.ch>
2026-08-06 15:48:03 +0200

    added caloParamsHI_2026_v0_2_cfi from 17_0_X, and added --tag option to viewCaloParams2.py

--- L1TriggerConfig/Utilities/test/viewCaloParams2.py
+++ L1TriggerConfig/Utilities/test/viewCaloParams2.py
@@ -15,6 +15,9 @@ group = parser.add_mutually_exclusive_group(required = True)
 group.add_argument('-g', '--globaltag', dest='globaltag', type=str, default=None,
                    help = 'Name of the GlobalTag')
 
+group.add_argument('-t', '--tag', dest='tag', type=str, default=None,
+                   help = 'Name of the L1TCaloParams tag to be read from the conditions database')
+
 group.add_argument('-c', '--caloParams-cfi', dest='caloParams_cfi', type=str, default=None,
                    help = 'Argument of process.load to load an instance of the "L1TCaloStage2ParamsESProducer" plugin')
 
@@ -27,10 +30,23 @@ process.maxEvents.input = 1
 from FWCore.Modules.modules import EmptySource
 process.source = EmptySource(firstRun = args.runNumber)
 
-if args.globaltag:
+def add_globaltag(globaltag):
     from Configuration.AlCa.GlobalTag import GlobalTag as customiseGlobalTag
     process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-    process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = args.globaltag)
+    process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = globaltag)
+
+if args.globaltag:
+    add_globaltag(args.globaltag)
+
+elif args.tag:
+    add_globaltag('161X_dataRun3_HLT_v1')
+    process.GlobalTag.toGet += [
+        cms.PSet(
+            record = cms.string("L1TCaloParamsRcd"),
+            tag = cms.string(args.tag),
+        )
+    ]
+
 else:
     process.load(args.caloParams_cfi)
 
